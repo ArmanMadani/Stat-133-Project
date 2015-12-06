55f050bf4c0e9f5b4174cef8a2d869bb08a682be
Eric Zhao <ericz82@gmail.com>
2015-12-05 16:36:25 -0800

    got player height data

--- Project Data Collection and Cleaning.R
+++ Project Data Collection and Cleaning.R
@@ -2,201 +2,39 @@ library(XML)
 
 # Data Frame of NBA Champions
 url.champs <- 'https://www.ticketcity.com/nba/nba-finals-tickets/nba-finals-champions.html'
-download.file(url, '~/Desktop/Stat 133 Project/Raw Data/champions.html')
-nba.champions <- readHTMLTable('~/Desktop/Stat 133 Project/Raw Data/champions.html')
+download.file(url, 'rawdata/champions.html')
+nba.champions <- readHTMLTable('rawdata/champions.html')
 nba.champions <- as.data.frame(nba.champions[[2]])
 nba.champions[] <- lapply(nba.champions, as.character)
 colnames(nba.champions) <- nba.champions[1, ]
 nba.champions <- nba.champions[-1, ]
 ## Cut this down to champions from 1995 - 2015
 
-# List of 'Top Ten' Draft Picks 
-# 1995
-url.draft.1995 <- 'http://www.nbadraft.net/nba_draft_history/1995.html'
-download.file(url.draft.1995, '~/Desktop/Stat 133 Project/Raw Data/1995draft.html')
-draft.1995 <- readHTMLTable('~/Desktop/Stat 133 Project/Raw Data/1995draft.html')
-draft.1995 <- as.data.frame(draft.1995[[1]])
-draft.1995[] <- lapply(draft.1995, as.character)
-colnames(draft.1995) <- draft.1995[1, ]
-draft.1995 <- draft.1995[-1, ]
-draft.1995 <- head(draft.1995, 10)
-# 1996
-url.draft.1996 <- 'http://www.nbadraft.net/nba_draft_history/1996.html'
-download.file(url.draft.1996, '~/Desktop/Stat 133 Project/Raw Data/1996draft.html')
-draft.1996 <- readHTMLTable('~/Desktop/Stat 133 Project/Raw Data/1996draft.html')
-draft.1996 <- as.data.frame(draft.1996[[1]])
-draft.1996[] <- lapply(draft.1996, as.character)
-colnames(draft.1996) <- draft.1996[1, ]
-draft.1996 <- draft.1996[-1, ]
-draft.1996 <- head(draft.1996, 10)
-# 1997
-url.draft.1995 <- 'http://www.nbadraft.net/nba_draft_history/1995.html'
-download.file(url.draft.1995, '~/Desktop/Stat 133 Project/Raw Data/1995draft.html')
-draft.1995 <- readHTMLTable('~/Desktop/Stat 133 Project/Raw Data/1995draft.html')
-draft.1995 <- as.data.frame(draft.1995[[1]])
-draft.1995[] <- lapply(draft.1995, as.character)
-colnames(draft.1995) <- draft.1995[1, ]
-draft.1995 <- draft.1995[-1, ]
-draft.1995 <- head(draft.1995, 10)
-# 1998
-url.draft.1995 <- 'http://www.nbadraft.net/nba_draft_history/1995.html'
-download.file(url.draft.1995, '~/Desktop/Stat 133 Project/Raw Data/1995draft.html')
-draft.1995 <- readHTMLTable('~/Desktop/Stat 133 Project/Raw Data/1995draft.html')
-draft.1995 <- as.data.frame(draft.1995[[1]])
-draft.1995[] <- lapply(draft.1995, as.character)
-colnames(draft.1995) <- draft.1995[1, ]
-draft.1995 <- draft.1995[-1, ]
-draft.1995 <- head(draft.1995, 10)
-# 1999
-url.draft.1995 <- 'http://www.nbadraft.net/nba_draft_history/1995.html'
-download.file(url.draft.1995, '~/Desktop/Stat 133 Project/Raw Data/1995draft.html')
-draft.1995 <- readHTMLTable('~/Desktop/Stat 133 Project/Raw Data/1995draft.html')
-draft.1995 <- as.data.frame(draft.1995[[1]])
-draft.1995[] <- lapply(draft.1995, as.character)
-colnames(draft.1995) <- draft.1995[1, ]
-draft.1995 <- draft.1995[-1, ]
-draft.1995 <- head(draft.1995, 10)
-# 2000
-url.draft.1995 <- 'http://www.nbadraft.net/nba_draft_history/1995.html'
-download.file(url.draft.1995, '~/Desktop/Stat 133 Project/Raw Data/1995draft.html')
-draft.1995 <- readHTMLTable('~/Desktop/Stat 133 Project/Raw Data/1995draft.html')
-draft.1995 <- as.data.frame(draft.1995[[1]])
-draft.1995[] <- lapply(draft.1995, as.character)
-colnames(draft.1995) <- draft.1995[1, ]
-draft.1995 <- draft.1995[-1, ]
-draft.1995 <- head(draft.1995, 10)
-# 2001
-url.draft.1995 <- 'http://www.nbadraft.net/nba_draft_history/1995.html'
-download.file(url.draft.1995, '~/Desktop/Stat 133 Project/Raw Data/1995draft.html')
-draft.1995 <- readHTMLTable('~/Desktop/Stat 133 Project/Raw Data/1995draft.html')
-draft.1995 <- as.data.frame(draft.1995[[1]])
-draft.1995[] <- lapply(draft.1995, as.character)
-colnames(draft.1995) <- draft.1995[1, ]
-draft.1995 <- draft.1995[-1, ]
-draft.1995 <- head(draft.1995, 10)
-# 2002
-url.draft.1995 <- 'http://www.nbadraft.net/nba_draft_history/1995.html'
-download.file(url.draft.1995, '~/Desktop/Stat 133 Project/Raw Data/1995draft.html')
-draft.1995 <- readHTMLTable('~/Desktop/Stat 133 Project/Raw Data/1995draft.html')
-draft.1995 <- as.data.frame(draft.1995[[1]])
-draft.1995[] <- lapply(draft.1995, as.character)
-colnames(draft.1995) <- draft.1995[1, ]
-draft.1995 <- draft.1995[-1, ]
-draft.1995 <- head(draft.1995, 10)
-# 2003
-url.draft.1995 <- 'http://www.nbadraft.net/nba_draft_history/1995.html'
-download.file(url.draft.1995, '~/Desktop/Stat 133 Project/Raw Data/1995draft.html')
-draft.1995 <- readHTMLTable('~/Desktop/Stat 133 Project/Raw Data/1995draft.html')
-draft.1995 <- as.data.frame(draft.1995[[1]])
-draft.1995[] <- lapply(draft.1995, as.character)
-colnames(draft.1995) <- draft.1995[1, ]
-draft.1995 <- draft.1995[-1, ]
-draft.1995 <- head(draft.1995, 10)
-# 2004
-url.draft.1995 <- 'http://www.nbadraft.net/nba_draft_history/1995.html'
-download.file(url.draft.1995, '~/Desktop/Stat 133 Project/Raw Data/1995draft.html')
-draft.1995 <- readHTMLTable('~/Desktop/Stat 133 Project/Raw Data/1995draft.html')
-draft.1995 <- as.data.frame(draft.1995[[1]])
-draft.1995[] <- lapply(draft.1995, as.character)
-colnames(draft.1995) <- draft.1995[1, ]
-draft.1995 <- draft.1995[-1, ]
-draft.1995 <- head(draft.1995, 10)
-# 2005
-url.draft.1995 <- 'http://www.nbadraft.net/nba_draft_history/1995.html'
-download.file(url.draft.1995, '~/Desktop/Stat 133 Project/Raw Data/1995draft.html')
-draft.1995 <- readHTMLTable('~/Desktop/Stat 133 Project/Raw Data/1995draft.html')
-draft.1995 <- as.data.frame(draft.1995[[1]])
-draft.1995[] <- lapply(draft.1995, as.character)
-colnames(draft.1995) <- draft.1995[1, ]
-draft.1995 <- draft.1995[-1, ]
-draft.1995 <- head(draft.1995, 10)
-# 2006
-url.draft.1995 <- 'http://www.nbadraft.net/nba_draft_history/1995.html'
-download.file(url.draft.1995, '~/Desktop/Stat 133 Project/Raw Data/1995draft.html')
-draft.1995 <- readHTMLTable('~/Desktop/Stat 133 Project/Raw Data/1995draft.html')
-draft.1995 <- as.data.frame(draft.1995[[1]])
-draft.1995[] <- lapply(draft.1995, as.character)
-colnames(draft.1995) <- draft.1995[1, ]
-draft.1995 <- draft.1995[-1, ]
-draft.1995 <- head(draft.1995, 10)
-# 2007
-url.draft.1995 <- 'http://www.nbadraft.net/nba_draft_history/1995.html'
-download.file(url.draft.1995, '~/Desktop/Stat 133 Project/Raw Data/1995draft.html')
-draft.1995 <- readHTMLTable('~/Desktop/Stat 133 Project/Raw Data/1995draft.html')
-draft.1995 <- as.data.frame(draft.1995[[1]])
-draft.1995[] <- lapply(draft.1995, as.character)
-colnames(draft.1995) <- draft.1995[1, ]
-draft.1995 <- draft.1995[-1, ]
-draft.1995 <- head(draft.1995, 10)
-# 2008
-url.draft.1995 <- 'http://www.nbadraft.net/nba_draft_history/1995.html'
-download.file(url.draft.1995, '~/Desktop/Stat 133 Project/Raw Data/1995draft.html')
-draft.1995 <- readHTMLTable('~/Desktop/Stat 133 Project/Raw Data/1995draft.html')
-draft.1995 <- as.data.frame(draft.1995[[1]])
-draft.1995[] <- lapply(draft.1995, as.character)
-colnames(draft.1995) <- draft.1995[1, ]
-draft.1995 <- draft.1995[-1, ]
-draft.1995 <- head(draft.1995, 10)
-# 2009
-url.draft.1995 <- 'http://www.nbadraft.net/nba_draft_history/1995.html'
-download.file(url.draft.1995, '~/Desktop/Stat 133 Project/Raw Data/1995draft.html')
-draft.1995 <- readHTMLTable('~/Desktop/Stat 133 Project/Raw Data/1995draft.html')
-draft.1995 <- as.data.frame(draft.1995[[1]])
-draft.1995[] <- lapply(draft.1995, as.character)
-colnames(draft.1995) <- draft.1995[1, ]
-draft.1995 <- draft.1995[-1, ]
-draft.1995 <- head(draft.1995, 10)
-# 2010
-url.draft.1995 <- 'http://www.nbadraft.net/nba_draft_history/1995.html'
-download.file(url.draft.1995, '~/Desktop/Stat 133 Project/Raw Data/1995draft.html')
-draft.1995 <- readHTMLTable('~/Desktop/Stat 133 Project/Raw Data/1995draft.html')
-draft.1995 <- as.data.frame(draft.1995[[1]])
-draft.1995[] <- lapply(draft.1995, as.character)
-colnames(draft.1995) <- draft.1995[1, ]
-draft.1995 <- draft.1995[-1, ]
-draft.1995 <- head(draft.1995, 10)
-# 2011
-url.draft.1995 <- 'http://www.nbadraft.net/nba_draft_history/1995.html'
-download.file(url.draft.1995, '~/Desktop/Stat 133 Project/Raw Data/1995draft.html')
-draft.1995 <- readHTMLTable('~/Desktop/Stat 133 Project/Raw Data/1995draft.html')
-draft.1995 <- as.data.frame(draft.1995[[1]])
-draft.1995[] <- lapply(draft.1995, as.character)
-colnames(draft.1995) <- draft.1995[1, ]
-draft.1995 <- draft.1995[-1, ]
-draft.1995 <- head(draft.1995, 10)
-# 2012
-url.draft.1995 <- 'http://www.nbadraft.net/nba_draft_history/1995.html'
-download.file(url.draft.1995, '~/Desktop/Stat 133 Project/Raw Data/1995draft.html')
-draft.1995 <- readHTMLTable('~/Desktop/Stat 133 Project/Raw Data/1995draft.html')
-draft.1995 <- as.data.frame(draft.1995[[1]])
-draft.1995[] <- lapply(draft.1995, as.character)
-colnames(draft.1995) <- draft.1995[1, ]
-draft.1995 <- draft.1995[-1, ]
-draft.1995 <- head(draft.1995, 10)
-# 2013
-url.draft.1995 <- 'http://www.nbadraft.net/nba_draft_history/1995.html'
-download.file(url.draft.1995, '~/Desktop/Stat 133 Project/Raw Data/1995draft.html')
-draft.1995 <- readHTMLTable('~/Desktop/Stat 133 Project/Raw Data/1995draft.html')
-draft.1995 <- as.data.frame(draft.1995[[1]])
-draft.1995[] <- lapply(draft.1995, as.character)
-colnames(draft.1995) <- draft.1995[1, ]
-draft.1995 <- draft.1995[-1, ]
-draft.1995 <- head(draft.1995, 10)
-# 2014
-url.draft.1995 <- 'http://www.nbadraft.net/nba_draft_history/1995.html'
-download.file(url.draft.1995, '~/Desktop/Stat 133 Project/Raw Data/1995draft.html')
-draft.1995 <- readHTMLTable('~/Desktop/Stat 133 Project/Raw Data/1995draft.html')
-draft.1995 <- as.data.frame(draft.1995[[1]])
-draft.1995[] <- lapply(draft.1995, as.character)
-colnames(draft.1995) <- draft.1995[1, ]
-draft.1995 <- draft.1995[-1, ]
-draft.1995 <- head(draft.1995, 10)
-# 2015
-url.draft.1995 <- 'http://www.nbadraft.net/nba_draft_history/1995.html'
-download.file(url.draft.1995, '~/Desktop/Stat 133 Project/Raw Data/1995draft.html')
-draft.1995 <- readHTMLTable('~/Desktop/Stat 133 Project/Raw Data/1995draft.html')
-draft.1995 <- as.data.frame(draft.1995[[1]])
-draft.1995[] <- lapply(draft.1995, as.character)
-colnames(draft.1995) <- draft.1995[1, ]
-draft.1995 <- draft.1995[-1, ]
-draft.1995 <- head(draft.1995, 10)
+# Player heights
+url.player_height <- 'http://www.draftexpress.com/nba-draft-history/?syear=1995'
+download.file(url.player_height, 'rawdata/player_heights1995.html')
+players <- readHTMLTable('rawdata/player_heights1995.html')
+players <- as.data.frame(players[[9]])
+players["year"] = 1995
+players <- players[c("Name", "Height", "Weight", "year")]
+
+for (i in 1996:2015) {
+  url.player_height <- paste0('http://www.draftexpress.com/nba-draft-history/?syear=', i)
+  download.file(url.player_height, paste0('rawdata/player_heights', i, '.html'))
+  p <- readHTMLTable(paste0('rawdata/player_heights', i, '.html'))
+  p <- as.data.frame(p[[9]])
+  p["year"] = i
+  p <- p[c("Name", "Height", "Weight", "year")]
+  players <- rbind(players, p)
+}
+
+to_inches <- function(height) {
+  h <- strsplit(as.character(height), "[^0-9]")
+  ft <- as.numeric(h[[1]][1])
+  inch <- as.numeric(h[[1]][2])
+  return(ft * 12 + inch)
+}
+
+players["Height"] <- unlist(lapply(players["Height"][[1]], to_inches))
+
+write.csv(players, file = "data/players.csv")
