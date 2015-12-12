747a521181cf56c2ced8168f2ce25fc43ccf4967
Eric Zhao <ericz82@gmail.com>
2015-12-11 17:26:54 -0800

    got averages

--- code/Data Cleaning and Preparation.R
+++ code/Data Cleaning and Preparation.R
@@ -30,19 +30,6 @@ all_champs <- all_champs[-seq(from = 2, to = nrow(all_champs), by = 2L), ]
 write.csv(all_champs, file = "data/champ_data.csv")
 
 # Drafted Players' Heights
-players <- readHTMLTable('rawdata/player_heights1995.html')
-players <- as.data.frame(players[[9]])
-players["year"] = 1995
-players <- players[c("Name", "Height", "Weight", "year")]
-
-for (i in 1996:2015) {
-  p <- readHTMLTable(paste0('rawdata/player_heights', i, '.html'))
-  p <- as.data.frame(p[[9]])
-  p["year"] = i
-  p <- p[c("Name", "Height", "Weight", "year")]
-  players <- rbind(players, p)
-}
-
 to_inches <- function(height) {
   h <- strsplit(as.character(height), "[^0-9]")
   ft <- as.numeric(h[[1]][1])
@@ -50,24 +37,47 @@ to_inches <- function(height) {
   return(ft * 12 + inch)
 }
 
+players <- readHTMLTable('rawdata/player_heights1995.html')
+players <- as.data.frame(players[[9]])
 players["Height"] <- unlist(lapply(players["Height"][[1]], to_inches))
+players["Year"] = 1995
+players["Average Height"] = mean(players$Height, na.rm = TRUE)
+players["Average Weight"] = mean(as.numeric(levels(players$Weight)), na.rm = TRUE)
+players <- players[c("Name", "Height", "Weight", 
+                     "Average Height", "Average Weight", "Year")]
+
+for (i in 1996:2015) {
+  p <- readHTMLTable(paste0('rawdata/player_heights', i, '.html'))
+  p <- as.data.frame(p[[9]])
+  p["Height"] <- unlist(lapply(p["Height"][[1]], to_inches))
+  p["Year"] = i
+  p["Average Height"] = mean(p$Height, na.rm = TRUE)
+  p["Average Weight"] = mean(as.numeric(levels(p$Weight)), na.rm = TRUE)
+  p <- p[c("Name", "Height", "Weight", 
+           "Average Height", "Average Weight", "Year")]
+  players <- rbind(players, p)
+}
 
 write.csv(players, file = "data/players.csv")
 
 # Cleaning data for champion rosters
 champ <- readHTMLTable('rawdata/champion_stats1995.html')
 champ <- as.data.frame(champ[[1]])
+champ["Ht"] <- unlist(lapply(champ["Ht"][[1]], to_inches))
 champ["Year"] = 1995
-champ <- champ[c("Player", "Pos", "Ht", "Wt", "Year")]
+champ["Average Height"] = mean(champ$Ht, na.rm = TRUE)
+champ["Average Weight"] = mean(as.numeric(levels(champ$Wt)), na.rm = TRUE)
+champ <- champ[c("Player", "Pos", "Ht", "Wt", "Average Height", "Average Weight", "Year")]
 
 for (i in 1996:2015) {
   ch <- readHTMLTable(paste0('rawdata/champion_stats', i, '.html'))
   ch <- as.data.frame(ch[[1]])
+  ch["Ht"] <- unlist(lapply(ch["Ht"][[1]], to_inches))
   ch["Year"] = i
-  ch <- ch[c("Player", "Pos", "Ht", "Wt", "Year")]
+  ch["Average Height"] = mean(ch$Ht, na.rm = TRUE)
+  ch["Average Weight"] = mean(as.numeric(levels(ch$Wt)), na.rm = TRUE)
+  ch <- ch[c("Player", "Pos", "Ht", "Wt", "Average Height", "Average Weight", "Year")]
   champ <- rbind(champ, ch)
 }
-
-champ["Ht"] <- unlist(lapply(champ["Ht"][[1]], to_inches))
 
 write.csv(champ, file = "data/roster.csv")
