7bc245ed9996f4b7f9fe7453e12cdaff4785e3a1
Eric Zhao <ericz82@gmail.com>
2015-12-13 17:22:13 -0800

    changed graphs a bit

--- code/Visualizations.R
+++ code/Visualizations.R
@@ -47,14 +47,14 @@ players_unique <- data.frame(unique(players$Year),
 qplot(x = unique(players$Year), y = unique(players$Average_Height), 
       data = players_unique, geom = c("point", "line"), ylab = "Height", 
       xlab = "Year", ylim = c(60, 96), 
-      main = 'Average Height of Draft Classes 1995 - 2015')
+      main = 'Average Height of Draft Classes')
 ggsave(file = "images/draft_height.pdf")
 ggsave(file = "images/draft_height.png")
 
 qplot(x = unique(players$Year), y = unique(players$Average_Weight),
       data = players_unique, geom = c("point", "line"), ylab = "Weight",
       xlab = "Year", ylim = c(133, 375),
-      main = "Average Weight of Draft Classes 1995 - 2015")
+      main = "Average Weight of Draft Classes")
 ggsave(file = "images/draft_weight.pdf")
 ggsave(file = "images/draft_weight.png")
 
@@ -82,45 +82,54 @@ ggsave(file = "images/champ_weight.png")
 
 # Champion 3-Pt FGA Year-to-Year
 qplot(x = years, y = X3PA, data = data, geom = c("point", "line"), 
-      ylab = "3-Pt FGA", main = 'Champions 3-Pt FGA Over Time')
+      ylab = "3-Pt FGA", main = 'Champions 3-Pt FGA Over Time') + 
+  theme(plot.title = element_text(size=12))
 
 # Champion 3-Pt FGP Year-to-Year
 qplot(x = years, y = X3PP, data = data, geom = c("point", "line"), 
       ylim = c(20, 50), ylab = "3 Pt. FG %", 
-      main = 'Champions 3-Pt. FG % Over Time')
+      main = 'Champions 3-Pt. FG % Over Time') +
+  theme(plot.title = element_text(size=12))
 
 # Champion Field Goal Attempts (FGA) Year-to-Year
 qplot(x = years, y = FGA, data = data, geom = c("point", "line"),
-      ylab = "Total FGA", main = 'Champions Total FGA Over Time')
+      ylab = "Total FGA", main = 'Champions Total FGA Over Time') +
+  theme(plot.title = element_text(size=12))
 
 # Champion Field Goal Percentage (FGP) Year-to-Year
 qplot(x = years, y = FGP, data = data, geom = c("point", "line"), 
       ylim = c(30, 60), ylab = "Total FG % ", 
-      main = 'Champions Total FG % Over Time')
+      main = 'Champions Total FG % Over Time') +
+  theme(plot.title = element_text(size=12))
 
 # 3-Pt Field Goals as a Percentage of Total Field Goals
 qplot(x = years, y = X3PtFGA, data = data, geom = c("point", "line"),
       ylim = c(10, 35), ylab = "3-Pt/Total FGA", 
-      main = 'Ratio of 3-Pt FG to Total FGA Over Time')
+      main = 'Ratio of 3-Pt FG to Total FGA Over Time') +
+  theme(plot.title = element_text(size=12))
 
 # Average height and weight of NBA Draft Picks
 qplot(x = unique(players$Year), y = unique(players$Average_Height), 
       data = players_unique, geom = c("point", "line"), ylab = "Height", 
       xlab = "Year", ylim = c(60, 96), 
-      main = 'Average Height of Draft Classes 1995 - 2015')
+      main = 'Average Height of Draft Classes') +
+  theme(plot.title = element_text(size=12))
 
 qplot(x = unique(players$Year), y = unique(players$Average_Weight),
       data = players_unique, geom = c("point", "line"), ylab = "Weight",
       xlab = "Year", ylim = c(133, 375),
-      main = "Average Weight of Draft Classes 1995 - 2015")
+      main = "Average Weight of Draft Classes") +
+  theme(plot.title = element_text(size=12))
 
 # Average height and weight of NBA Championship Lineups
 qplot(x = unique(champ$Year), y = unique(champ$Average_Height),
       data = champ_unique, geom = c("point", "line"), ylab = "Height", 
       xlab = "Year", main = "Average Height of Championship Lineup",
-      ylim = c(60, 96))
+      ylim = c(60, 96)) +
+  theme(plot.title = element_text(size=12))
 
 qplot(x = unique(champ$Year), y = unique(champ$Average_Weight),
       data = champ_unique, geom = c("point", "line"), ylab = "Weight",
       xlab = "Year", main = "Average Weight of Championship Lineup", 
-      ylim = c(133, 375))
+      ylim = c(133, 375)) +
+  theme(plot.title = element_text(size=12))
